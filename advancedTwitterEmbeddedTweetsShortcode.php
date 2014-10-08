24dca70288ab57b86f04a3bf3701012c9b0628ed
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-08 13:29:32 +0000

    information readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/advanced-twitter-embedded-tweets-shortcode/trunk@1003825 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advancedTwitterEmbeddedTweetsShortcode.php
+++ advancedTwitterEmbeddedTweetsShortcode.php
@@ -31,6 +31,6 @@ add_shortcode('embedTweet', 'advancedTwitterEmbeddedTweetsShortcode');
 		$data = "";
         $data .= $html;
 		if($author == "true"){
-			$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.liveherechicago.com' target='_blank' style='color: #808080;' title='Live Here Chicago'>Click here</a></div>";}
+			$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.garretcordwerner.com' target='_blank' style='color: #808080;' title='click here'>Seattle Architect</a></div>";}
     return $data;
  }
