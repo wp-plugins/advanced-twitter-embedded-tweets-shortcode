25d7a0e5839cfd25c8c54f43be6939be61728215
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-04 07:32:40 +0000

    changes up
    git-svn-id: https://plugins.svn.wordpress.org/advanced-twitter-embedded-tweets-shortcode/trunk@1152651 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advancedTwitterEmbeddedTweetsShortcode.php
+++ advancedTwitterEmbeddedTweetsShortcode.php
@@ -31,6 +31,6 @@ add_shortcode('embedTweet', 'advancedTwitterEmbeddedTweetsShortcode');
 		$data = "";
         $data .= $html;
 		if($author == "true"){
-			$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.tensunitdepot.com/' target='_blank' style='color: #808080;' title='visit the homepage'>tens units</a></div>";}
+			$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.tensunitdepot.com/' target='_blank' style='color: #808080;' title='visit the website'>click here</a></div>";}
     return $data;
  }
